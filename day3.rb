b6b363e0216e2035ae83f6089ec8ec88ba652963
Jacob Krall <jacob@jkrall.net>
2021-12-04 21:02:26 -0500

    Day 3, Part 2

--- day3.rb
+++ day3.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-diagnostics = ARGF.to_a
-len = diagnostics[0].chomp.length
+diagnostics = ARGF.to_a.map(&:chomp)
+len = diagnostics[0].length
 
 def tally(array)
   # TODO: in ruby 2.7 this should be built into Enumerable
@@ -25,3 +25,20 @@ end.join.to_i(2)
 puts "epsilon=#{epsilon}, gamma=#{gamma}. power=#{epsilon * gamma}"
 
 puts 'Part 2'
+
+def find_by(diagnostics, len)
+  len.times.inject(diagnostics) { |remaining, pos|
+    return remaining if remaining.length == 1
+    t = tally(remaining.map { |diag| diag[pos] })
+    remaining.select { |d| yield d, t, pos }
+  }
+end
+
+o2 = find_by(diagnostics, len) { |diag, tallies, pos|
+  diag[pos] == (tallies['0'] > tallies['1'] ? '0' : '1')
+}.first.to_i(2)
+co2 = find_by(diagnostics, len) { |diag, tallies, pos|
+  diag[pos] == (tallies['1'] < tallies['0'] ? '1' : '0')
+}.first.to_i(2)
+life = o2 * co2
+puts "o2=#{o2} co2=#{co2} life=#{life}"
