5a6d6f6a254bd12135cb9df9e7f5c95d36debe47
Jacob Krall <jacob@spreedly.com>
2021-12-08 14:37:13 -0500

    Day 8, part 2. Hugely inefficient (17.70s)

--- day8.rb
+++ day8.rb
@@ -1,6 +1,21 @@
 #!/usr/bin/env ruby
 
+require 'set'
+
 class Day8
+  NUMERALS = [
+    [0, 1, 2, 4, 5, 6],
+    [2, 5],
+    [0, 2, 3, 4, 6],
+    [0, 2, 3, 5, 6],
+    [1, 2, 3, 5],
+    [0, 1, 3, 5, 6],
+    [0, 1, 3, 4, 5, 6],
+    [0, 2, 5],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 5, 6]
+  ]
+
   attr_reader :list
   def initialize(lines)
     @list = lines
@@ -8,7 +23,11 @@ class Day8
       .map { |item|
         item
           .split(' | ')
-          .map { |sec| sec.split(' ') }
+          .map { |sec|
+            sec
+              .split(' ')
+              .map { |digit| digit.chars.sort.join }
+          }
       }
   end
 
@@ -17,7 +36,30 @@ class Day8
       output.count { |digit| [2, 3, 4, 7].include? digit.length }
     }
   end
+
+  def part2
+    list.sum { |patterns, output| decode(patterns, output) }
+  end
+
+  def decode(patterns, output)
+    h = deduce(patterns)
+    numerals = output.map { |num| h.find_index(num) }
+    numerals.join.to_i
+  end
+
+  def deduce(patterns)
+    patterns = patterns.to_set
+    'abcdefg'
+      .chars
+      .permutation
+      .map { |perm|
+        NUMERALS.map { |segments| segments.map { |seg| perm[seg] }.sort.join }
+      }
+      .select { |would_be| would_be.to_set == patterns }
+      .first
+  end
 end
 
 day8 = Day8.new(ARGF)
 p :part1, day8.part1
+p :part2, day8.part2
