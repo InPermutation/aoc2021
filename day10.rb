aaf683026b4a8c2d23d22639c32b634c95340afb
Jacob Krall <jacob@spreedly.com>
2021-12-10 10:25:46 -0500

    Day 10, part 2

--- day10.rb
+++ day10.rb
@@ -8,6 +8,14 @@ class Day10
     '}' => 1197,
     '>' => 25137
   }.freeze
+
+  AUTOCOMPLETE_SCORE = {
+    ')' => 1,
+    ']' => 2,
+    '}' => 3,
+    '>' => 4
+  }.freeze
+
   CHUNK_PAIRS = {
     '(' => ')',
     '[' => ']',
@@ -16,11 +24,24 @@ class Day10
   }.freeze
 
   def part1
-    errs = lines
+    lines
       .map(&method(:syntax_check))
       .select(&method(:corrupt?))
       .map { |_, wrong| SYNTAX_ERROR_SCORE[wrong] }
       .sum
+  end
+
+  def part2
+    scores = lines
+      .map(&method(:syntax_check))
+      .select(&method(:incomplete?))
+      .map { |_, stack|
+        stack.reverse.reduce(0) { |score, char|
+          score * 5 + AUTOCOMPLETE_SCORE[CHUNK_PAIRS[char]]
+        }
+      }
+
+    scores.sort[scores.length / 2]
   end
 
   private
@@ -37,14 +58,22 @@ class Day10
       if CHUNK_PAIRS.include?(ch)
         stack.push(ch)
       elsif stack.empty?
-        return [:incomplete, stack]
+        return [:underflow, ch, stack]
       elsif CHUNK_PAIRS[stack.pop] == ch
         next
       else
         return [:corrupt, ch]
       end
     end
-    return [:complete]
+    if stack.empty?
+      return [:complete]
+    else
+      return [:incomplete, stack]
+    end
+  end
+
+  def underflow?(state)
+    state[0] == :underflow
   end
 
   def corrupt?(state)
@@ -62,3 +91,4 @@ end
 
 day10 = Day10.new(ARGF.map(&:chomp))
 p :part1, day10.part1
+p :part2, day10.part2
