e7e4aa8e00d2120bb5a43022c3d603a921a054d5
Jacob Krall <jacob@spreedly.com>
2021-12-11 14:15:23 -0500

    Day 11: part 1

--- day11.rb
+++ day11.rb
@@ -3,6 +3,18 @@
 
 class Day11
   def part1
+    grid = initial_energy.map(&:dup)
+    flashes = 0
+    100.times do
+      increase_by_1!(grid)
+      loop do
+        f = flash!(grid)
+        flashes += f
+        break if f.zero?
+      end
+    end
+
+    flashes
   end
 
   def part2
@@ -16,7 +28,48 @@ class Day11
     @initial_energy = lines.map do |line|
       line.chars.map(&:to_i).to_a.freeze
     end.to_a.freeze
-    p @initial_energy
+  end
+
+  def indexes(grid)
+    grid.length.times.flat_map do |y|
+      grid[y].length.times.map do |x|
+        [x, y]
+      end
+    end
+  end
+
+  NEIGHBOR_DIRECTIONS =
+    [-1, 0, 1].product([-1, 0, 1]) - [[0, 0]]
+
+  def neighbors(x, y)
+    NEIGHBOR_DIRECTIONS.map { |dx, dy| [x + dx, y + dy] }
+  end
+
+  def eligible_to_propagate?(grid, x, y)
+    return false if x.negative? || y.negative?
+
+    grid.at(y)
+      &.at(x)
+      &.positive?
+  end
+
+  def increase_by_1!(grid)
+    indexes(grid).each { |x, y| grid[y][x] += 1 }
+  end
+
+  def flash!(grid)
+    flashes = 0
+    indexes(grid).each do |x, y|
+      next unless grid[y][x] > 9
+
+      grid[y][x] = 0
+      flashes += 1
+
+      neighbors(x, y)
+        .select { |x1, y1| eligible_to_propagate?(grid, x1, y1) }
+        .each { |x1, y1| grid[y1][x1] += 1 }
+    end
+    flashes
   end
 end
 
