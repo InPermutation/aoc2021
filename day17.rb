f73574f7cdc1117bcecb96ee711259453291bace
Jacob Krall <jacob@spreedly.com>
2021-12-17 11:01:29 -0500

    Day 17: rubocop

--- day17.rb
+++ day17.rb
@@ -15,12 +15,12 @@ class Day17
   private
 
   attr_reader :target
+
   Target = Struct.new(:x, :y)
 
   def hits
     res = []
     (target.y.min..1000).each do |vyi| # TODO: is 1000 correct?
-      puts vyi
       (0..target.x.max).each do |vxi|
         probe = Probe.new(vxi, vyi)
         res << probe if ever_hits?(probe)
@@ -33,18 +33,19 @@ class Day17
     loop do
       return true if hit(probe)
       return false if missed(probe)
+
       probe.step!
     end
   end
 
   def hit(probe)
-    return target.x.include?(probe.x) && target.y.include?(probe.y)
+    target.x.include?(probe.x) && target.y.include?(probe.y)
   end
 
   def missed(probe)
     return true if probe.y < target.y.min && probe.vx.negative?
     return true if probe.x > target.x.max
-    return false unless probe.vx == 0
+    return false unless probe.vx.zero?
     return true if probe.x < target.x.min
     return false unless probe.vy.negative?
 
@@ -58,9 +59,9 @@ class Day17
       @x += vx
       @y += vy
       @max_y = @y if @y > @max_y
-      if vx > 0
+      if vx.positive?
         @vx -= 1
-      elsif vx < 0
+      elsif vx.negative?
         @vx += 1
       end
       @vy -= 1
@@ -71,24 +72,25 @@ class Day17
       "Probe<pos=(#{x}, #{y}) vel=(#{vx}, #{vy}) max_y=#{max_y}>"
     end
 
-    alias_method :to_s, :inspect
+    alias to_s inspect
 
     private
 
-    def initialize(vx, vy)
+    def initialize(vxi, vyi)
       @x = @y = @max_y = 0
-      @vx = vx
-      @vy = vy
+      @vx = vxi
+      @vy = vyi
     end
   end
 
   def initialize(lines)
     raise StandardError, 'wrong # of lines' if lines.count != 1
+
     xrange, yrange = lines[0]
-      .delete_prefix('target area: ')
-      .split(', ')
-      .map { |boundstr| boundstr.split('=')[1].split('..').map(&:to_i) }
-      .map { |min, max| Range.new(min, max) }
+                     .delete_prefix('target area: ')
+                     .split(', ')
+                     .map { |boundstr| boundstr.split('=')[1].split('..').map(&:to_i) }
+                     .map { |min, max| Range.new(min, max) }
     @target = Target.new(xrange, yrange)
   end
 end
