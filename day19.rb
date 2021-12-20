28d51b5fc6933e3c7ffb241c1735b673f2967648
Jacob Krall <jacob@spreedly.com>
2021-12-20 10:41:09 -0500

    Day 19: wip

--- day19.rb
+++ day19.rb
@@ -1,10 +1,33 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'matrix'
+
 class Day19
   def part1
-    solved = scanners.first
+    solved = scanners.take(1)
     unsolved = scanners.drop(1)
+    while unsolved.any?
+      find_match!(solved, unsolved)
+    end
+  end
+
+  def find_match!(solved, unsolved)
+    p :find_match!, solved.length, unsolved.length
+    unsolved.each do |unsolved_scanner|
+      unsolved_scanner.possible_orientations.each.with_index do |unsolved_orientation, i|
+        p un: i
+        solved.each do |solved_scanner|
+          overlap = solved_scanner.biggest_overlaps(unsolved_orientation)
+          if overlap.length > 12
+            solved.add(unsolved_orientation)
+            unsolved.remove(unsolved_scanner)
+            return
+          end
+        end
+      end
+    end
+    raise NotImplementedError, "couldn't find any matches"
   end
 
   def part2
@@ -14,6 +37,31 @@ class Day19
 
   class Scanner
     attr_reader :name, :beacons
+
+    ORDERINGS = [0, 1, 2].permutation(3).to_a.uniq.freeze
+    REFLECTIONS = [-1, 1].product([-1, 1]).product([-1, 1]).map(&:flatten).to_a.freeze
+    ALL_ROTATIONS = ORDERINGS.product(REFLECTIONS).freeze
+    def possible_orientations
+      ALL_ROTATIONS.map { |ordering, reflection|
+        n = beacons.map { |b|
+          bo = Vector.elements(b.to_a.values_at(*ordering))
+          Vector.elements(bo.zip(reflection).map { |c, x| c * x })
+        }
+        Scanner.new(name, n)
+      }
+    end
+
+    def biggest_overlaps(other)
+      possible_offsets = beacons.product(other.beacons).map { |s, u| u - s }.uniq
+
+      overlaps = possible_offsets.map do |diff|
+        #p diff: diff, first_beacon: beacons[0], first_other: other.beacons[0], d: other.beacons[0] - diff
+        opoints = other.beacons.map { |beac| beac - diff }
+        opoints & beacons
+      end
+      p count_overlaps: overlaps.map(&:length).uniq
+      overlaps.max_by(&:length)
+    end
 
     private
 
@@ -37,7 +85,7 @@ class Day19
         name = line.gsub('---', '').strip.freeze
         beacons = []
       else
-        beacons << line.split(',').map(&:to_i).map(&:freeze)
+        beacons << Vector.elements(line.split(',').map(&:to_i))
       end
     end
     @scanners = scanners.freeze
