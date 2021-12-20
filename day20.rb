f62973f2580ea9926be07bc1f2f3370c56707989
Jacob Krall <jacob@spreedly.com>
2021-12-20 12:08:22 -0500

    Day 20: part 1

--- day20.rb
+++ day20.rb
@@ -4,11 +4,11 @@
 class Day20
   def part1
     img = @input_image
-    2.times do |n|
+    2.times do
       img = enhance(img)
       self.class.debug_image(img)
     end
-    img.length
+    img.values.select(&self.class.method(:light?)).length
   end
 
   def part2; end
@@ -16,27 +16,35 @@ class Day20
   private
 
   def enhance(img)
-    res = {}
+    res = Hash.new(img.default)
     self.class.bounds(img).each do |coord|
       nv = self.class.window_centered(coord, img)
-      enhanced = enhancement_algorithm[nv]
-      res[coord] = enhanced if enhanced
+      res[coord] = enhancement_algorithm[nv]
     end
-    res
+    res.default = case res.default
+                  when '.'
+                    enhancement_algorithm[0]
+                  when '#'
+                    enhancement_algorithm[511]
+                  else
+                    raise NotImplementedError, res.default
+                  end
+    res.freeze
   end
 
-  def light?(ch)
+  def self.light?(ch)
     ch == '#'
-  end
-
-  def dark?(ch)
-    ch == '.'
   end
 
   WINDOW_OFFSETS = [-1, 0, 1].product([-1, 0, 1]).map(&:freeze).freeze
   def self.window_centered(coord, img)
     cx, cy = coord
-    WINDOW_OFFSETS.map { |dy, dx| img[[cx + dx, cy + dy]] ? '1' : '0' }.to_a.join.to_i(2)
+    WINDOW_OFFSETS
+      .map { |dy, dx| img[[cx + dx, cy + dy]] }
+      .map { |v| light?(v) ? 1 : 0 }
+      .to_a
+      .join
+      .to_i(2)
   end
 
   def self.bounds(img)
@@ -58,9 +66,10 @@ class Day20
   end
 
   def self.debug_image(img)
+    return
     bounds_y(img).each do |y|
       bounds_x(img).each do |x|
-        printf img[[x, y]] ? '#' : '.'
+        printf (img[[x, y]]).to_s
       end
       puts
     end
@@ -69,16 +78,15 @@ class Day20
   attr_reader :enhancement_algorithm, :input_image
 
   def initialize(lines)
-    @enhancement_algorithm = lines[0].chars.map(&method(:light?)).freeze
-    raise StandardError, 'oh_no' if @enhancement_algorithm.first && @enhancement_algorithm.last
+    @enhancement_algorithm = lines[0].freeze
+    raise StandardError, 'oh_no' if self.class.light?(@enhancement_algorithm[0]) &&
+                                    self.class.light?(@enhancement_algorithm[-1])
     raise StandardError, "512 != #{@enhancement_algorithm.length}" unless @enhancement_algorithm.length == 512
 
-    @input_image = {}
+    @input_image = Hash.new('.')
     lines.drop(2).each.with_index do |line, y|
       line.chars.each.with_index do |ch, x|
-        if light?(ch)
-          input_image[ [x, y] ] = true
-        end
+        input_image[[x, y]] = ch
       end
     end
     @input_image = input_image.freeze
@@ -88,5 +96,4 @@ end
 
 day20 = Day20.new(ARGF.map(&:chomp).freeze)
 p part1: day20.part1
-puts "(5294 is too low)"
 p part2: day20.part2
