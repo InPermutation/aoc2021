524e0adb1ae5c3b664ca8421ab19cd9e63c6e0f8
Jacob Krall <jacob@spreedly.com>
2021-12-22 14:38:11 -0500

    Day 22: part 2 - takes way too long (1683.44s)

--- day22.rb
+++ day22.rb
@@ -1,39 +1,133 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'set'
+class Cuboid
+  # [xmin..xmax, ymin..ymax, zmin..zmax]
+  attr_reader :ranges
+
+  def intersect?(other)
+    ranges.zip(other.ranges).all? do |rc1, rc2|
+      rc1.max >= rc2.min && rc1.min <= rc2.max
+    end
+  end
+
+  def fully_contains?(other)
+    ranges.zip(other.ranges).all? do |rc1, rc2|
+      rc1.min <= rc2.min && rc1.max >= rc2.max
+    end
+  end
+
+  def inspect
+    "<Cuboid[#{ranges[0]}, #{ranges[1]}, #{ranges[2]}]>"
+  end
+
+  def init_procedure?
+    ranges.none? { |r| r.min < -50 || r.max > 50 }
+  end
+
+  def except(other)
+    what = ranges
+      .zip(other.ranges)
+      .map(&Cuboid.method(:possibly))
+
+    intersection = what[0].product(*what.drop(1))
+    while intersection.delete(other.ranges); end
+    cuboids = intersection
+      .map { |rangecube| Cuboid.new(rangecube) }
+      .to_a
+  end
+
+  def self.possibly(range2)
+    my_range, their_range = range2
+    if my_range.max < their_range.min || my_range.min > their_range.max
+      raise "err possibly #{range2}"
+    elsif my_range.min < their_range.min && my_range.max > their_range.max
+      return legal_ranges(*[
+        (my_range.min .. (their_range.min - 1)),
+        (their_range),
+        (their_range.max + 1)..my_range.max
+      ])
+    elsif my_range.min >= their_range.min && my_range.max <= their_range.max
+      return [my_range]
+    elsif my_range.min < their_range.min
+      return legal_ranges((my_range.min..(their_range.min - 1)), (their_range.min..my_range.max))
+    elsif my_range.max >= their_range.max
+      return legal_ranges((my_range.min..their_range.max), ((their_range.max + 1)..my_range.max))
+    else
+      raise "oh no"
+    end
+  end
+
+  def self.legal_ranges(*ranges)
+    ranges.select { _1.count.positive? }
+  end
+
+  def count
+    ranges.map(&:count).reduce(&:*)
+  end
+
+  alias :to_s :inspect
+
+  private
+
+  def initialize(ranges)
+    @ranges = ranges.freeze
+  end
+end
 
 class Day22
   def part1
-    on = Set.new
-    commands
-      .select { |_, rangecube| init_procedure?(rangecube) }
-      .each do |cmd, rangecube|
-        points = all_points(*rangecube)
-        case cmd
-        when 'on'
-          on.merge(points)
-        when 'off'
-          on.subtract(points)
-        else
-          raise StandardError, cmd
-        end
-      end
-    on.length
+    init_commands = commands
+      .select { |_, cuboid| cuboid.init_procedure? }
+    execute(init_commands).sum(&:count)
   end
 
   def part2
-    raise NotImplementedError, :part2
+    execute(commands).sum(&:count)
   end
 
   private
 
-  def init_procedure?(rangecube)
-    rangecube.none? { |r| r.min < -50 || r.max > 50 }
+  def execute(cmds)
+    i = 0
+
+    cmds.reduce([]) do |on_cuboids, command|
+      puts "execute #{command} (#{i += 1}/#{cmds.length}) (on_cuboids.length = #{on_cuboids.length})"
+      cmd, cuboid = command
+      case cmd
+      when 'on'
+        on_cuboids = switch_on(on_cuboids, cuboid)
+      when 'off'
+        on_cuboids = switch_off(on_cuboids, cuboid)
+      end
+    end
   end
 
-  def all_points(xr, yr, zr)
-    xr.to_a.product(yr.to_a, zr.to_a).map(&:freeze).freeze
+  def switch_on(on_cuboids, *cuboids)
+    on_cuboids.each do |already_on|
+      cuboids = cuboids.reject(&already_on.method(:fully_contains?))
+      cuboids.each do |to_switch|
+        if already_on.intersect?(to_switch)
+          raise "whoops" unless cuboids.delete(to_switch)
+          cuboids += to_switch.except(already_on)
+          return switch_on(on_cuboids, *cuboids)
+        end
+      end
+    end
+    on_cuboids + cuboids
+  end
+
+  def switch_off(on_cuboids, cuboid)
+    on_cuboids.each do |already_on|
+      if cuboid.intersect?(already_on)
+        raise "whoops" unless on_cuboids.delete(already_on)
+        unless cuboid.fully_contains?(already_on)
+          on_cuboids += already_on.except(cuboid)
+        end
+        return switch_off(on_cuboids, cuboid)
+      end
+    end
+    on_cuboids
   end
 
   attr_reader :commands
@@ -50,7 +144,7 @@ class Day22
             Range.new(*range.split('..').map(&:to_i))
           end
 
-        [cmd.freeze, rangecube.freeze].freeze
+        [cmd.freeze, Cuboid.new(rangecube)].freeze
       end.to_a.freeze
   end
 end
